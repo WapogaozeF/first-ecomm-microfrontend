d5991d46405673babbffc3bbc9e9becdbf4e1e04
Verner Ravil <werner.ravil@gmail.com>
2023-03-01 21:53:03 +0500

    fix faker function

--- cart/src/index.js
+++ cart/src/index.js
@@ -1,5 +1,5 @@
 import {faker} from '@faker-js/faker';
 
-const cartText = `<div>You have ${faker.random.number()} items in your cart</div>`;
+const cartText = `<div>You have ${faker.datatype.number()} items in your cart</div>`;
 
 document.querySelector('#cart-dev').innerHTML = cartText;
