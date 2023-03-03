3d040376cc377b50d93d03b21cbd34817b61007b
Verner Ravil <werner.ravil@gmail.com>
2023-03-03 20:41:57 +0500

    add envirement test in bootsrap in products

--- products/src/bootstrap.js
+++ products/src/bootstrap.js
@@ -11,4 +11,12 @@ const mount = (el) => {
     el.innerHTML = products;
 };
 
+if (process.env.NODE_ENV === "development") {
+    const el = document.querySelector('#dev-products');
+
+    if (el) {
+        mount(el);
+    }
+}
+
 export { mount };
